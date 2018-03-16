172fb88b076bb9f906d5c1299e70cf30b1aaccea
bartnaessens <bart.naessens@gmail.com>
2018-03-16 09:07:05 +0100

    Add CLS awareness (#51)
    * Add CLS awareness
    
    * Directly inspect Sequelize for cls transactions, no longer needed to pass the namespace as parameter.

--- src/index.js
+++ src/index.js
@@ -209,7 +209,7 @@ function shimModel(target) {
       if ([null, undefined].indexOf(id) !== -1) {
         return Promise.resolve(null);
       }
-      if (options.transaction || options.include) {
+      if (options.transaction || options.include || activeClsTransaction()) {
         return original.apply(this, arguments);
       }
 
@@ -229,7 +229,7 @@ function shimBelongsTo(target) {
 
   shimmer.wrap(target, 'get', original => {
     return function batchedGetBelongsTo(instance, options = {}) {
-      if (Array.isArray(instance) || options.include || options.transaction) {
+      if (Array.isArray(instance) || options.include || options.transaction || activeClsTransaction()) {
         return original.apply(this, arguments);
       }
 
@@ -256,7 +256,7 @@ function shimHasOne(target) {
 
   shimmer.wrap(target, 'get', original => {
     return function batchedGetHasOne(instance, options = {}) {
-      if (Array.isArray(instance) || options.include || options.transaction) {
+      if (Array.isArray(instance) || options.include || options.transaction || activeClsTransaction()) {
         return original.apply(this, arguments);
       }
 
@@ -277,7 +277,7 @@ function shimHasMany(target) {
   shimmer.wrap(target, 'get', original => {
     return function bathedGetHasMany(instances, options = {}) {
       let isCount = false;
-      if (options.include || options.transaction) {
+      if (options.include || options.transaction || activeClsTransaction()) {
         return original.apply(this, arguments);
       }
 
@@ -345,7 +345,7 @@ function shimBelongsToMany(target) {
       let isCount = false;
       assert(this.paired, '.paired missing on belongsToMany association. You need to set up both sides of the association');
 
-      if (options.include || options.transaction) {
+      if (options.include || options.transaction || activeClsTransaction()) {
         return original.apply(this, arguments);
       }
 
@@ -426,6 +426,18 @@ let GLOBAL_CACHE;
 export function resetCache() {
   if (GLOBAL_CACHE) GLOBAL_CACHE.reset();
 }
+
+function activeClsTransaction() {
+  if (/^4/.test(Sequelize.version)) {
+    if (Sequelize._cls && Sequelize._cls.get('transaction')) {
+      return true;
+    }
+  } else if (Sequelize.cls && Sequelize.cls.get('transaction')) {
+    return true;
+  }
+  return false;
+}
+
 export default function (target, options = {}) {
   options = {
     ...options,
---
+++ test/integration/transaction.test.js
@@ -0,0 +1,104 @@
+import {connection, randint} from '../helper';
+import Sequelize from 'sequelize';
+import sinon from 'sinon';
+import dataloaderSequelize from '../../src';
+import expect from 'unexpected';
+import Promise from 'bluebird';
+import cls from 'continuation-local-storage';
+
+describe('Transactions', function () {
+  beforeEach(async function () {
+    this.sandbox = sinon.sandbox.create();
+  });
+  afterEach(function () {
+    this.sandbox.restore();
+  });
+
+  describe('Managed', function () {
+    beforeEach(async function () {
+      this.sandbox = sinon.sandbox.create();
+
+      this.User = connection.define('user');
+    
+      dataloaderSequelize(this.User);
+
+      await connection.sync({
+        force: true
+      });
+
+      this.users = await this.User.bulkCreate([
+        { id: randint() },
+        { id: randint() }
+      ], { returning: true });
+
+      this.sandbox.spy(this.User, 'findAll');
+    });
+
+    it('does not batch during managed transactions', async function () {
+      let user1, user2;
+      await connection.transaction(async (t) => {
+        [user1, user2] = await Promise.all([this.User.findById(this.users[1].get('id'), {transaction: t}),
+          this.User.findById(this.users[0].get('id'), {transaction: t})]);
+      });
+      expect(user1, 'to equal', this.users[1]);
+      expect(user2, 'to equal', this.users[0]);
+
+      expect(this.User.findAll, 'not to have calls satisfying', [{
+        where: {
+          id: [this.users[1].get('id'), this.users[0].get('id')]
+        }
+      }]);
+    });
+  });
+
+  describe('CLS', function () {
+    beforeEach(async function () {
+      this.namespace = cls.createNamespace('sequelize');
+      if (/^4/.test(Sequelize.version)) {
+        Sequelize.useCLS(this.namespace);
+      } else {
+        Sequelize.cls = this.namespace;
+      }
+      this.sandbox = sinon.sandbox.create();
+      
+      this.User = connection.define('user');
+    
+      dataloaderSequelize(this.User);
+
+      await connection.sync({
+        force: true
+      });
+
+      this.users = await this.User.bulkCreate([
+        { id: randint() },
+        { id: randint() }
+      ], { returning: true });
+
+      this.sandbox.spy(this.User, 'findAll');
+    })
+
+    after(function () {
+      if (/^4/.test(Sequelize.version)) {
+        delete Sequelize._cls;
+      } else {
+        delete Sequelize.cls;
+      }
+    })
+
+    it('does not batch during CLS transactions', async function () {
+      let user1, user2;
+      await connection.transaction(async (t) => {
+        [user1, user2] = await Promise.all([this.User.findById(this.users[1].get('id')),
+          this.User.findById(this.users[0].get('id'))]);
+      });
+      expect(user1, 'to equal', this.users[1]);
+      expect(user2, 'to equal', this.users[0]);
+
+      expect(this.User.findAll, 'not to have calls satisfying', [{
+        where: {
+          id: [this.users[1].get('id'), this.users[0].get('id')]
+        }
+      }]);
+    });
+  });
+});
