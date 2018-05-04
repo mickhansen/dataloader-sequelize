cc2266a002eb3bb574300f91436eaae781f2c5b8
François Hodierne <francois@hodierne.net>
2018-05-04 08:06:03 +0200

    convert $and operator alias to [Sequelize.Op.and] (#52)
    * convert $and alias operator to [Sequelize.Op.and]
    
    Operators alias are already deprecated in Sequelize 4 and it's already advised to run without it.
    
    In Sequelize 5 they will be completely gone.
    
    This change removes the $and operator and uses the future-proof syntax.
    
    * support Sequelize 3.x which doesn't provide Op

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ export function getCacheKey(model, attribute, options) {
 function mergeWhere(where, optionsWhere) {
   if (optionsWhere) {
     return {
-      $and: [where, optionsWhere]
+      [Sequelize.Op ? Sequelize.Op.and : '$and']: [where, optionsWhere]
     };
   }
   return where;
